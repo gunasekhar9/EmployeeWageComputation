4ee97beadfc7aaeebec1f8fe717867120eccb9b4
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-03 19:56:32 +0530

    [gunasekhar9] add : Daily Working Wages of Employee

--- EmployeeWage/EmployeeWage/DailyEmployeeWage.cs
+++ EmployeeWage/EmployeeWage/DailyEmployeeWage.cs
@@ -4,22 +4,26 @@ using System.Text;
 
 namespace EmployeeWage
 {
-        public class EmployeeWageComputation
+    public class EmployeeWageComputation
+    {
+
+        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, WAGE_PER_HR = 20, FULL_WORKING_HRS = 8, PART_WORKING_HRS = 4;
+        int totalEmpWage, empHrs = 0;
+        public void DailyEmployeeWage()
         {
-            public const int IS_PRESENT = 1, WAGE_PER_HR = 20, FULL_WORKING_HRS = 8;
-            int totalEmpWage;
-            public void DailyEmployeeWage()
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_FULL_TIME)
             {
-                Random random = new Random();
-                int empCheck = random.Next(0, 2);
-                if (empCheck == IS_PRESENT)
-                {
-                    this.totalEmpWage = FULL_WORKING_HRS * WAGE_PER_HR;
-                    Console.WriteLine("Employee is present");
-                    Console.WriteLine(this.totalEmpWage);
-                }
-                else
-                    Console.WriteLine("Employee is absent");
+                this.empHrs = FULL_WORKING_HRS;
+                Console.WriteLine("Employee worked Full time");
             }
+            if (empCheck == IS_PART_TIME)
+            {
+                this.empHrs = PART_WORKING_HRS;
+                Console.WriteLine("Employee worked Part time");
+            }
+            Console.WriteLine(this.totalEmpWage = this.empHrs * WAGE_PER_HR);
         }
+    }
 }
