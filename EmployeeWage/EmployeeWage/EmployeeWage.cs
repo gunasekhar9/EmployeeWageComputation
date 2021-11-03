ec01497fca217c9954bedb044a3c7966b57efe92
gunasekhar9 <gunasekhar1539@gmail.com>
2021-11-03 14:41:43 +0530

    [gunasekhar9] add : Employee Wage Computation

---
+++ EmployeeWage/EmployeeWage/EmployeeWage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    public class EmployeeWageComputation
+    {
+        public const int IS_PRESENT = 1;
+        public void EmployeeAttendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);//0,1
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee Present");
+            else
+                Console.WriteLine("Employee Absent");
+        }
+
+    }
+}
---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+            EmployeeWageComputation employee = new EmployeeWageComputation();
+            employee.EmployeeAttendance();
+
+        }
+    }
+}
